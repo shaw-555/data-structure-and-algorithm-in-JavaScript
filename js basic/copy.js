4e09f193dec118340af5abacf1d34797c618484a
shaw <shaw18664590570@163.com>
2020-12-13 20:35:04 +0800

    fix: upload the code

--- js basic/copy.js
+++ js basic/copy.js
@@ -26,3 +26,17 @@ changeObj(obj);
 console.log('引用类型进入函数后，传入的是引用类型的地址，所以对参数的更改会作用到其本身',obj);
 console.log('参数在函数内指向另一个数组后，则不再指向先前的地址，进而指向新的地址，所以会返回push后的数组'+test);
 
+
+var array = [1,2,3,{}];
+var arrayItemLast = array[3];
+var arrayItemFirst = array[0];
+var newArray = array;
+
+delete array[3];
+console.log(newArray);
+console.log(arrayItemLast);
+delete array[0];
+console.log(newArray);
+console.log(arrayItemFirst,'arrayItemFirst 所拿的值是一个原始值，所以得到的是该值的副本，故不会受delete操作影响');
+
+
